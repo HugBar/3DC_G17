2ba116603caa6b4126b133eebe1f3c623b4fa72e
pedroazevedo17 <1221264@isep.ipp.pt>
2024-11-13 14:45:27 +0000

    US 6.2.6 #9
    more updates

--- my-react-app/src/components/Patient/createPatient/CreatePatient.js
+++ my-react-app/src/components/Patient/createPatient/CreatePatient.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import usePatientFormValidation from '../../../hooks/usePatientFormValidation';
-import patientService from '../../../api/patientService';
 import './CreatePatient.css';
 
 const CreatePatient = () => {
